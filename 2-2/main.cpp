6e20b477fe805208af92e2e8bbc26a7641ff1dab
rlul <45661770+rlul@users.noreply.github.com>
2023-12-01 18:05:58 +0300

    Fixed a bug, added optimisations

--- 2-2/main.cpp
+++ 2-2/main.cpp
@@ -7,23 +7,60 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <format>
+
+#define REPLACE_TEST(h, n, r, e)	\
+	{	\
+		std::string result = replace_needles(h, n, r);	\
+		std::cout << std::format("{:s}: \"{}\" == \"{}\"", result == (e), result, e) << std::endl; \
+	}	\
 
 std::string replace_needles(const std::string& haystack, const std::string& needle, const std::string& replacement)
 {
-	std::string result{haystack};
+	std::stringstream result;
 	size_t pos = 0;
 
-	while (pos != std::string::npos)
+	if (needle.empty()) 
 	{
-		result.replace(pos, needle.length(), replacement);
-		pos = result.find(needle, pos + needle.size());
+		return haystack;
 	}
 
-	return result;
+	while (pos < haystack.length()) {
+		const size_t new_pos = haystack.find(needle, pos);
+
+		if (new_pos == std::string::npos) 
+		{
+			result << haystack.substr(pos);
+			break;
+		}
+
+		result << haystack.substr(pos, new_pos - pos) << replacement;
+		pos = new_pos + needle.length();
+	}
+
+	return result.str();
 }
 
 int main(int argc, char** argv)
 {
+	REPLACE_TEST("123 aa123 123aa", "123", "bbb", "bbb aabbb bbbaa");
+	REPLACE_TEST("aaa123 123 aa123", "123", "bbb", "aaabbb bbb aabbb");
+
+	REPLACE_TEST("123 aa123 123aa", "12", "bbb", "bbb3 aabbb3 bbb3aa");
+	REPLACE_TEST("123 aa123 123aa", "123", "bb", "bb aabb bbaa");
+
+	REPLACE_TEST("aaa123 123 aa123", "12", "bbb", "aaabbb3 bbb3 aabbb3");
+	REPLACE_TEST("aaa123 123 aa123", "123", "bb", "aaabb bb aabb");
+
+	REPLACE_TEST("123 aaa123 123123", "123", "bb", "bb aaabb bbbb");
+	REPLACE_TEST("123 aaa123 123123", "12", "bbb", "bbb3 aaabbb3 bbb3bbb3");
+
+	REPLACE_TEST("", "123", "bbb", "");
+	REPLACE_TEST("123 aaa123 123123", "", "bbb", "123 aaa123 123123");
+	REPLACE_TEST("123 aaa123 123123", "123", "", " aaa ");
+	REPLACE_TEST("123 aaa123 123123", "123", "123", "123 aaa123 123123");
+
 	std::string haystack, needle, replacement;
 
 	std::cout << "Haystack: ";
